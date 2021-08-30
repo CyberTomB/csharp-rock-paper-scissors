0219bf59cf50c683794e395f85d47f345f894988
CyberTomB <afntwrk.guyrpg@gmail.com>
2021-08-30 15:44:36 -0600

    game only accepts valid strings

--- Game/Game.cs
+++ Game/Game.cs
@@ -9,16 +9,26 @@ namespace csharp_rps
     public void StartGame()
     {
       System.Console.WriteLine("Let's play rock-paper-scissors!");
-      foreach (string i in RPS){
+      foreach (string i in RPS)
+      {
         System.Console.WriteLine("{0} ", i);
       }
+      PlayerChoice();
+
+    }
+
+    private void PlayerChoice()
+    {
       string playerChoice = Console.ReadLine().ToLower();
       string match = RPS.Find(s => s == playerChoice);
-      if(match == null){
-        System.Console.WriteLine("That's not a valid choice.");
-      } else {System.Console.WriteLine("You chose: " + match);}
-      
+      if (match == null)
+      {
+        System.Console.WriteLine("That's not a valid choice. Try again, noob:");
+        PlayerChoice();
+      }
+      else { System.Console.WriteLine("You chose: " + match); }
     }
+
     public Game()
     {
       RPS = new List<String>{"rock", "paper", "scissors"};
